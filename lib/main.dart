4596884aead41810ea136bfb46693cb4d804b260
성영욱노트북 <bareman9@naver.com>
2022-05-19 20:38:35 +0900

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         fontFamily: "jua",
         primarySwatch: Colors.blue,
